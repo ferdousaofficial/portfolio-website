8146fd823305c5dc77fcf6769561202b59a36977
theferdousa <ferdousaofficial@gmail.com>
2023-12-09 05:04:44 +0600

    fix: fixed issue

--- src/config/Icons.ts
+++
@@ -1,8 +0,0 @@
-// import icons from react-icons
-import { HiOutlineMail } from "react-icons/hi";
-import { IconsTypes } from "./types";
-
-// icon export
-export const icons: IconsTypes = {
-  mail: HiOutlineMail,
-};
--- src/config/types.ts
+++ src/config/types.ts
@@ -1,10 +1,3 @@
-import { IconType } from "react-icons";
-
-// Define interface for icons
-export interface IconsTypes {
-  [key: string]: IconType;
-}
-
 // define interface for nav items
 export interface navItemsTypes {
   id: number;
